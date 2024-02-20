a513c4482c8acf38fa2631ed019ed5748c24dcc9
kdando <kvdando@gmail.com>
2024-02-20 10:58:52 +0000

    basic app-level error handling

--- app.js
+++ app.js
@@ -11,4 +11,23 @@ app.use("/api", apiRouter);
 
 //////////////////////////////
 
+////ERROR HANDLING////
+//400 Bad Path
+app.use((req, res, next) => {
+    return res.status(400).send({ msg: "Path does not exist."});
+})
+
+//Error received from model
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        return res.status(err.status).send(err);
+    }
+    return res.status(404).send({ msg: "Not found."});
+})
+
+//"Catch all bucket" for other errors
+app.use("*", (err, req, res, next) => {
+    return res.status(500).send({ msg: "Internal server error."})
+})
+
 module.exports = app;
